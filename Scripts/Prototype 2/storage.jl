5f00b82a9c7759a80323db0b6502dc695e27f09d
Luca Mihailescu <mihailescu.luca@gmail.com>
2020-07-17 17:46:56 +0100

    Implemented Pajek export

--- Scripts/Prototype 2/storage.jl
+++ Scripts/Prototype 2/storage.jl
@@ -2,7 +2,7 @@ using CSV
 using DataFrames
 using Dates
 using LightGraphs
-using GraphPlot, Compose
+using GraphIO
 
 function createDataFrame()
     global Data = DataFrame(E=[0], c1=[0], c2=[0], c3=[0], c4=[0], c5=[0], c6=[0], c7=[0], c8=[0], c9=[0], c10=[0])
@@ -13,6 +13,7 @@ function exportData(dir)
     mkdir(string("Data/",dir))
     CSV.write(string("Data/",dir,"/","data.csv"), Data)
     
-    #Exporting the graph doesn't work for some reason:
-    #draw(PNG(string("Data\\",exportTime,"\\","graph.png")), gplot(Network))
+    #Exporting the graph
+    global Network
+    savegraph(string("Data/",dir,"/","graph.net"), Network, "Network", GraphIO.NET.NETFormat())
 end
